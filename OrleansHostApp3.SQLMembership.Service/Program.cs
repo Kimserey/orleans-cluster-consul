e19b0fc61bc5dc11d4edf5cee533076c02e395d1
kimserey <kimserey.lam@groomgy.com>
2017-10-03 09:29:27 +0800

    Take port from args

--- OrleansHostApp3.SQLMembership.Service/Program.cs
+++ OrleansHostApp3.SQLMembership.Service/Program.cs
@@ -11,9 +11,10 @@ namespace OrleansHostApp3.SQLMembership.Service
     {
         public static void Main(string[] args)
         {
+            var port = args.Length > 0 ? args[0] : "20080";
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://localhost:20080")
+                .UseUrls($"http://localhost:{port}")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
