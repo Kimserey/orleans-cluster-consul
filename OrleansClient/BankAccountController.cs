02a20df4b1ede0150c29375296cb79a562407e92
kimserey <kimserey.lam@groomgy.com>
2017-10-06 08:23:45 +0800

    Added client for storage

---
+++ OrleansClient/BankAccountController.cs
@@ -0,0 +1,29 @@
+﻿using GrainInterface;
+using Microsoft.AspNetCore.Mvc;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrleansClient
+{
+    [Route("BankAccount")]
+    public class BankAccountController: Controller
+    {
+        private IGrainFactory _factory;
+
+        public BankAccountController(IGrainFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [HttpPost("{cardNumber}")]
+        public async Task<IActionResult> AddCard(string cardNumber)
+        {
+            var grain = _factory.GetGrain<IBankAccountGrain>(Guid.Empty, "bankAccount");
+            await grain.AddCard(cardNumber);
+            return Ok();
+        }
+    }
+}
---
+++ OrleansClient/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Runtime.Configuration;
+using System.Net;
+
+namespace OrleansClient
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IGrainFactory>(sp =>
+            {
+                var client =
+                    new ClientBuilder()
+                        .UseConfiguration(new ClientConfiguration
+                        {
+                            DeploymentId = "main",
+                            Gateways = new List<IPEndPoint>{
+                                new IPEndPoint(IPAddress.Loopback, 40023)
+                            }
+                        })
+                        .Build();
+
+                client.Connect().Wait();
+
+                return client;
+            });
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole().AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.ApplicationServices.GetRequiredService<IGrainFactory>();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
